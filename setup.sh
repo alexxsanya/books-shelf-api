9715c2388e3ff60dd44b54a2978d529b2e79b554
alexxsanya <alexxsanya@gmail.com>
2019-10-16 17:50:54 +0300

    add shell script to create setup db with data

---
+++ setup.sh
@@ -0,0 +1,3 @@
+service postgresql start
+su - postgres bash -c "psql < /home/alex/Documents/Udacity/projects/bookshelf/db_setup.sql"
+su - postgres bash -c "psql bookshelf < /home/alex/Documents/Udacity/projects/bookshelf/books.psql"
