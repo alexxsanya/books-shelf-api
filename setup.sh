90ce08d475a32142b6b7b89f1d0f4e2ddd7e833e
alexxsanya <alexxsanya@gmail.com>
2019-10-17 14:23:17 +0300

    add a script dump sample data in test db

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 service postgresql start
 su - postgres bash -c "psql < /home/alex/Documents/Udacity/projects/bookshelf/db_setup.sql"
 su - postgres bash -c "psql bookshelf < /home/alex/Documents/Udacity/projects/bookshelf/books.psql"
+su - postgres bash -c "psql bookshelf_test < /home/alex/Documents/Udacity/projects/bookshelf/books.psql"
